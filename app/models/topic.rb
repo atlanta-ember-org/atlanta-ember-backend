853eb594dabf0062a82625f7df27ffbb6cae4075
Patrick <pdougall1@gmail.com>
2015-08-22 15:37:58 -0400

    Adds associations between topic and user through assignments

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,6 @@
 class Topic < ActiveRecord::Base
   validates_uniqueness_of :name
   validates_presence_of :name
+  has_many :assignments
+  has_many :users, through: :assignments
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,4 +9,25 @@ describe User do
     end
   end
 
+  context "with topics" do
+    let!(:topic_1) {
+      topic = FactoryGirl.create(:topic, name: 'topic_1')
+      subject.assignments.create(topic: topic)
+      topic
+    }
+    let!(:topic_2) {
+      topic = FactoryGirl.create(:topic, name: 'topic_2')
+      subject.assignments.create(topic: topic)
+      topic
+    }
+    let!(:topic_3) {
+      topic = FactoryGirl.create(:topic, name: 'topic_3')
+      subject.assignments.create(topic: topic)
+      topic
+    }
+
+    it "can be assigned to many topics" do
+      expect(subject.topics.map(&:name)).to eql(['topic_1', 'topic_2', 'topic_3'])
+    end
+  end
 end
