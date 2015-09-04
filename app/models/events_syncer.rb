886f9b1bd57cb28ec53990c460e16ee7efe59376
Patrick Dougall <pdougall1@gmail.com>
2015-09-04 19:22:00 -0400

    Don't add events if ther are present

--- app/models/events_syncer.rb
+++ app/models/events_syncer.rb
@@ -6,7 +6,11 @@ class EventsSyncer
 
   def sync
     parsed_events.each do |parsed_event|
-      Event.create!(parsed_event.to_hash)
+      find_by_params = meetup_id: parsed_event.meetup_id
+      Event.find_or_create_by(find_by_params) do |event|
+        event.update_attributes!(parsed_event.to_hash)
+        event.save
+      end
     end
   end
 
