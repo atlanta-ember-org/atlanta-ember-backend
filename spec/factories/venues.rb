719cbd842432bbccec4f4e5354464f78f93c1634
Patrick Dougall <pdougall1@gmail.com>
2015-09-07 19:58:33 -0400

    Adds a venue model

---
+++ spec/factories/venues.rb
@@ -0,0 +1,15 @@
+FactoryGirl.define do
+  factory :venue do
+    country "MyString"
+city "MyString"
+starts_at "2015-09-04 22:43:09"
+address_1 "MyString"
+yes_rsvp_count 1
+name "MyString"
+lon 1
+lat 1
+repinned false
+uid "MyString"
+  end
+
+end
