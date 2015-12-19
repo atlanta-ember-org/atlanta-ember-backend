ec1a6576811ab49ef3fba505fae3318f73ad4157
Patrick Dougall <pdougall1@gmail.com>
2015-12-19 13:51:24 -0500

    Creates a token when session is created

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,3 +1,11 @@
 class Session < ActiveRecord::Base
   belongs_to :user
+  before_create :create_token
+
+  private
+
+  def create_token
+    @token = Auth::Token.new
+    self.auth_token = @token.to_hash
+  end
 end
--- spec/models/session_spec.rb
+++ spec/models/session_spec.rb
@@ -4,4 +4,9 @@ describe Session do
   subject(:session) { FactoryGirl.create(:session) }
 
   it { is_expected.to respond_to :user }
+
+  describe '#auth_token' do
+    subject { session.auth_token }
+    it { is_expected.not_to be nil }
+  end
 end
