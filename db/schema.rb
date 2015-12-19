3ded8c0521a5aa4b0dda066369904417185118d1
Chris McCuller <cmcculler@salesforce.com>
2015-12-19 14:39:37 -0500

    Re-enable postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151217195324) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "assignments", force: true do |t|
     t.integer  "user_id"
     t.integer  "topic_id"
