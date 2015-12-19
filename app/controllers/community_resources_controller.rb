3e225a69a028d8645e3679828108af7077acc304
Chris McCuller <cmcculler@salesforce.com>
2015-12-19 16:19:30 -0500

    PUT requests :boom:

--- app/controllers/community_resources_controller.rb
+++ app/controllers/community_resources_controller.rb
@@ -1,28 +1,37 @@
 class CommunityResourcesController < ApplicationController
-	def create
-      resource = CommunityResource.new(community_resource_params)
-      if resource.save
-        status = :ok
-      else
-        status = :bad_request
-      end
-      head status
+  before_filter :set_resource, :except => [:index, :create]
+  def set_resource
+    @resource = CommunityResource.find(params['id'])
+  end
+
+  def create
+    resource = CommunityResource.new(community_resource_params)
+    if resource.save
+      status = :ok
+    else
+      status = :bad_request
     end
+    head status
+  end
 
-    def index
-      @resources = CommunityResource.all
-      render json: @resources
-    end
+  def index
+    @resources = CommunityResource.all
+    render json: @resources
+  end
 
-	def destroy
-		resource = CommunityResource.find(params['id'])
-		resource.delete
-		head :ok
-	end
+  def destroy
+    @resource.delete
+    head :ok
+  end
 
-    private
+  def update
+    @resource.update_attributes(community_resource_params)
+    render json: @resource
+  end
 
-    def community_resource_params
-      params.require(:community_resource).permit(:title, :url, :description, :group)
-    end
+  private
+
+  def community_resource_params
+    params.require(:community_resource).permit(:title, :url, :description, :group, :is_approved)
+  end
 end
--- spec/requests/community_resources_spec.rb
+++ spec/requests/community_resources_spec.rb
@@ -1,39 +1,54 @@
 require "spec_helper"
 
 describe "Community resources" do
-	describe "post create" do
-		it "creates a new community resource" do
-			params = {
-				community_resource: {
-					title: 'A blog post',
-					url: 'http://www.example.com/a/post',
-					description: 'Super good info!',
-					group: 'Tutorial'
-				}
-			}
-			expect{ post community_resources_path, params }.to change{
-          	CommunityResource.count
-        	}.by 1
-		end
-	end
+  describe "post create" do
+    it "creates a new community resource" do
+      params = {
+        community_resource: {
+          title: 'A blog post',
+          url: 'http://www.example.com/a/post',
+          description: 'Super good info!',
+          group: 'Tutorial'
+        }
+      }
+      expect{ post community_resources_path, params }.to change{
+        CommunityResource.count
+      }.by 1
+    end
+  end
 
-	describe "get index" do
-		it "lists all community resources" do
-			CommunityResource.create!(title: 'A cool resource')
-			CommunityResource.create!(title: 'Another cool one')
-			get community_resources_path
-			expect(response.status).to eq 200
-			community_resources = JSON.parse(response.body)
-			expect(community_resources['community_resources'].length).to be 2
-		end
-	end
+  describe "get index" do
+    it "lists all community resources" do
+      CommunityResource.create!(title: 'A cool resource')
+      CommunityResource.create!(title: 'Another cool one')
+      get community_resources_path
+      expect(response.status).to eq 200
+      body = JSON.parse(response.body)
+      expect(body['community_resources'].length).to be 2
+    end
+  end
 
-	describe "destroy a resource" do
-		it "destroys an existing record" do
-			cr = CommunityResource.create!(title: 'A cool resource')
-			expect{ delete community_resource_path(cr) }.to change{
-				CommunityResource.count
-			}.by -1
-		end
-	end
+  describe "destroy a resource" do
+    it "destroys an existing record" do
+      cr = CommunityResource.create!(title: 'A cool resource')
+      expect{ delete community_resource_path(cr) }.to change{
+        CommunityResource.count
+      }.by -1
+    end
+  end
+
+  describe "update a resource" do
+    it "updates an existing record" do
+      params = {
+        community_resource: {
+          title: 'A blog post'
+        }
+      }
+      cr = CommunityResource.create!(title: 'A cool resource')
+      put community_resource_path(cr), params
+      expect(response.status).to be 200
+      body = JSON.parse(response.body)
+      expect(body["community_resource"]["title"]).to eq "A blog post"
+    end
+  end
 end
