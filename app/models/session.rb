fc074ce83add3b590a233df53185b2ddcdee5816
Patrick Dougall <pdougall1@gmail.com>
2015-12-21 12:11:52 -0500

    Adds a `token_valid?` method to session

--- app/models/session.rb
+++ app/models/session.rb
@@ -2,10 +2,20 @@ class Session < ActiveRecord::Base
   belongs_to :user
   before_create :create_token
 
+  attr_reader :new_token
+
+  def token_valid?(matching_token)
+    token_factory.new(matching_token).matches?(self.auth_token)
+  end
+
   private
 
   def create_token
-    @token = Auth::Token.new
-    self.auth_token = @token.to_hash
+    @new_token = token_factory.new
+    self.auth_token = @new_token.to_hash
+  end
+
+  def token_factory
+    Auth::Token
   end
 end
--- spec/models/session_spec.rb
+++ spec/models/session_spec.rb
@@ -1,12 +1,25 @@
 require 'spec_helper'
 
 describe Session do
-  subject(:session) { FactoryGirl.create(:session) }
-
-  it { is_expected.to respond_to :user }
+  let(:user) { FactoryGirl.create(:user) }
+  subject(:session) { FactoryGirl.create(:session, user: user) }
 
   describe '#auth_token' do
     subject { session.auth_token }
     it { is_expected.not_to be nil }
+  end
+
+  describe '#token_valid?(matching_token)' do
+    subject { session.token_valid?(matching_token) }
+
+    context 'when token matches' do
+      let(:matching_token) { session.new_token.to_s }
+      it { is_expected.to be true }
+    end
+
+    context 'when token does not match' do
+      let(:matching_token) { "wrong token" }
+      it { is_expected.to be false }
+    end
   end
 end
