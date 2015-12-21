c6faa33652e5581210f9700533ef5ec57509a395
Patrick Dougall <pdougall1@gmail.com>
2015-12-21 11:28:31 -0500

    Adds active field to session

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151219182335) do
+ActiveRecord::Schema.define(version: 20151221162631) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20151219182335) do
     t.string   "auth_token"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "active"
   end
 
   create_table "topics", force: true do |t|
