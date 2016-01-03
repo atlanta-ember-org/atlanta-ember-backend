644d3984745a6540398cd9af678fd0ad9926f50e
Patrick Dougall <pdougall1@gmail.com>
2016-01-03 14:32:12 -0500

    Adds password attribute to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151221162631) do
+ActiveRecord::Schema.define(version: 20151227002030) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 20151221162631) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "meetup_id"
+    t.string   "password"
   end
 
   create_table "venues", force: true do |t|
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,7 @@ FactoryGirl.define do
   factory :user do
     first_name "Example"
     last_name "User"
+    password "password"
+    password_confirmation "password"
   end
 end
